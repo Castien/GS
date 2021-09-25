407e6fef79052416ed273cc5e07b8c04c5f1d9e5
Eileen Lowers <eileenlowers@outlook.com>
2021-09-25 18:11:36 -0400

    updated DragonCave and GuessTheNumber with exceptions

--- DragonCave/src/DragonCave.java
+++ DragonCave/src/DragonCave.java
@@ -8,16 +8,20 @@ public class DragonCave {
                         "Which cave will you go into? (1 or 2)");
 
         Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
+        try {
+            int i = scan.nextInt();
 
-        if(i == 1) {
-            System.out.println("You approach the cave... \nIt is dark and spooky... \n" +
-                    "A large dragon jumps out in front of you! \nHe opens his jaws and...\n" +
-                    "Gobbles you down in one bite!");
-        }else {
-            System.out.println("You approach the cave... \nIt is dark and spooky... \n" +
-                    "A large dragon jumps out in front of you! \nRumbling a great laugh, he looks upon you and\n" +
-                    "Showers you with jewels and golden treasure!");
+            if (i == 1) {
+                System.out.println("You approach the cave... \nIt is dark and spooky... \n" +
+                        "A large dragon jumps out in front of you! \nHe opens his jaws and...\n" +
+                        "Gobbles you down in one bite!");
+            } else {
+                System.out.println("You approach the cave... \nIt is dark and spooky... \n" +
+                        "A large dragon jumps out in front of you! \nRumbling a great laugh, he looks upon you and\n" +
+                        "Showers you with jewels and golden treasure!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- GuessTheNumber/src/GuessTheNumber.java
+++ GuessTheNumber/src/GuessTheNumber.java
@@ -1,10 +1,10 @@
-import java.util.Locale;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
         Random randomnum = new Random();
 
@@ -13,28 +13,38 @@ public class GuessTheNumber {
         System.out.println("Hello! What is your name?");
         String name = scan.nextLine();
 
-        while(playing) {
+        while (playing) {
             int answer = randomnum.nextInt(20);
             int guess = 0;
             int tries = 0;
+
             System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.");
 
             while (tries < 6) {
                 System.out.println("Take a guess.");
-                guess = scan.nextInt();
-                tries++;
+                try {
+                    guess = scan.nextInt();
+                    tries++;
 
-                if (guess < answer) {
-                    System.out.println("Your guess is too low.");
+                    if(guess < 1 || guess > 20) {
+                        throw new InputMismatchException();
+                    }
+                    else {
+                        if (guess < answer) {
+                            System.out.println("Your guess is too low.");
+                        }
+                        if (guess > answer) {
+                            System.out.println("Your guess is too high");
+                        }
+                        if (guess == answer) {
+                            break;
+                        }
+                    }
                 }
-                if (guess > answer) {
-                    System.out.println("Your guess is too high");
-                }
-                if (guess == answer) {
-                    break;
+                catch (InputMismatchException mm) {
+                    System.out.println("Not a valid number.");
                 }
             }
-
             if(guess > 0) {
                 if (guess == answer) {
                     System.out.println("Good job, " + name + "! You guessed my number in " + tries + " guesses!");
@@ -43,16 +53,29 @@ public class GuessTheNumber {
                 }
             }
 
-            System.out.println("Would you like to play again? (Enter 'y' or 'n':)");
             scan.nextLine();
-            String restart = scan.nextLine();
+            System.out.println("Would you like to play again? (Enter 'y' or 'n':)");
+            playAgain(scan, playing);
+        }
+    }
 
-            if(!restart.equalsIgnoreCase("y")) {
-                if(restart.equalsIgnoreCase("n")) {
+    private static boolean playAgain(Scanner scan, boolean playing) {
+
+        do {
+            try {
+                String restart = scan.nextLine();
+                if (restart.equalsIgnoreCase("y")) {
+                    break;
+                } else if (restart.equalsIgnoreCase("n")) {
                     System.out.println("Thanks for playing!");
                     playing = false;
+                } else {
+                    throw new NoSuchElementException();
                 }
+            } catch (NoSuchElementException ns) {
+                System.out.println("Must choose 'y' or 'n'.");
             }
-        }
+        } while (true);
+        return playing;
     }
 }
